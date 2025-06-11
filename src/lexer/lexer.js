b6bdd3980ef8e2f3bd285b8b385863bb6ce1dd9e
angleyanalbedo <100198247+angleyanalbedo@users.noreply.github.com>
2025-06-11 21:40:19 +0800

    换antlr

--- src/lexer/lexer.js
+++
@@ -1,8 +0,0 @@
-import {Lexer} from "chevrotain";
-import {allTokens} from "./token.js";
-import { stParser } from "../parser/paser.js";
-
-
-
-//   Identifier, Integer, Real,
-export const lexer = new Lexer(allTokens);
