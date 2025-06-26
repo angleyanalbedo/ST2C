644217593ec6a542834ec7afc618f50fa37a8cd6
angleyanalbedo <100198247+angleyanalbedo@users.noreply.github.com>
2025-06-26 16:09:24 +0800

    第一次可以成功运行

--- st2c/Program.cs
+++ st2c/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace st2c
+﻿using Antlr4.Runtime;
+
+namespace st2c
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            PLCSTPARSERLexer pLCSTPARSERLexer = new PLCSTPARSERLexer(new AntlrInputStream(Console.In));
+            CommonTokenStream commonTokenStream = new CommonTokenStream(pLCSTPARSERLexer);
+            PLCSTPARSERParser pLCSTPARSERParser = new PLCSTPARSERParser(commonTokenStream);
+            pLCSTPARSERParser.RemoveErrorListeners();
+            pLCSTPARSERParser.AddErrorListener(new ConsoleErrorListener<IToken>());
+            var tree = pLCSTPARSERParser.startpoint();
+            if (tree != null)
+            {
+                Console.WriteLine("Parsing completed successfully.");
+                Console.WriteLine(tree.ToStringTree(pLCSTPARSERParser));
+            }
+            else
+            {
+                Console.WriteLine("Parsing failed.");
+            }
         }
     }
 }
